4fb50997d15c33f5e061b6c8c73ed70f542708c7
Ricardo Alexandre <silva1620costa@gmail.com>
2024-11-25 21:37:28 -0400

    Aula 03 inicio comit

---
+++ anime-service/src/main/java/estudodevdojo/spring/AnimeServeApplication.java
@@ -0,0 +1,13 @@
+package estudodevdojo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeServeApplication.class, args);
+	}
+
+}
---
+++ anime-service/src/test/java/estudodevdojo/spring/AnimeServeApplicationTests.java
@@ -0,0 +1,13 @@
+package estudodevdojo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
